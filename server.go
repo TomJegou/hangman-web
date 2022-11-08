79d301cbf1ae20931480fe903cf97b9a3d9ca9fb
TomJegou <tomjegou10@gmail.com>
2022-11-08 11:45:17 +0100

    server ready

--- server.go
+++ server.go
@@ -5,9 +5,14 @@ import (
 	"net/http"
 )
 
+func hangHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	//http.HandleFunc("/hangman", )
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
