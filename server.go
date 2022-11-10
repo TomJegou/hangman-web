3676a3a85f520d32b8e302b3155424a1330eb2b6
fpierrea <pierrealexis.favril@ynov.com>
2022-11-10 09:49:40 +0100

    first display

--- server.go
+++ server.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func hangHandler(w http.ResponseWriter, r *http.Request) {
+type Hangman struct {
+	WordToDisplay string
+}
 
+func hangHandler(w http.ResponseWriter, r *http.Request) {
+	data := Hangman{
+		WordToDisplay: "Hangman"}
+	tmpl, err := template.ParseFiles("static/début.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	tmpl.Execute(w, data)
 }
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	//http.HandleFunc("/hangman", )
+	http.HandleFunc("/hangman", hangHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
