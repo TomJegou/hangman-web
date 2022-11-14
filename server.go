35b12100f9f3a167365043f42c66a3bf7381ec54
TomJegou <tomjegou10@gmail.com>
2022-11-14 09:22:51 +0100

    modif server adding goroutines

--- server.go
+++ server.go
@@ -1,26 +1,51 @@
 package main
 
 import (
+	"hangman"
 	"html/template"
 	"log"
 	"net/http"
+	"sync"
 )
 
 type Hangman struct {
 	WordToDisplay string
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("static/début.html"))
-	data := Hangman{
-		WordToDisplay: "hangman",
+var Input string
+var Content string
+
+func input(wg *sync.WaitGroup, inputChan chan<- string, responseChan <-chan string) {
+	defer wg.Done()
+	for content := range responseChan {
+		Content = content
+		inputChan <- Input
 	}
-	tmpl.Execute(w, data)
 }
 
-func main() {
-	http.HandleFunc("/", Handler)
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("static/hangmanweb.html"))
+	data := Hangman{
+		WordToDisplay: Content,
+	}
+	tmpl.Execute(w, data)
+	Input = r.FormValue("input")
+}
+
+func Server() {
+	http.HandleFunc("/", rootHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	inputChan := make(chan string)
+	responseChan := make(chan string)
+	wg.Add(2)
+	go input(&wg, inputChan, responseChan)
+	go Server()
+	go hangman.Hangman(&wg, inputChan, responseChan)
+	wg.Wait()
 }
