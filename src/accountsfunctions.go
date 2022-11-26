a701027692d0d89ec53adc9c832fe78ad28fa305
TomJegou <tomjegou10@gmail.com>
2022-11-26 15:25:00 +0100

    Commenting function

--- src/accountsfunctions.go
+++ src/accountsfunctions.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+// Stuctures
 type User struct {
 	Name   string
 	Passwd string
@@ -19,6 +20,7 @@ type UserList struct {
 var Current_User User
 var User_list UserList
 
+/*Save the list of users in the accounts.json file*/
 func saveUserList() {
 	bytevalue, err := json.MarshalIndent(User_list, "", "	")
 	if err != nil {
@@ -27,6 +29,7 @@ func saveUserList() {
 	os.WriteFile("db/accounts.json", bytevalue, 0644)
 }
 
+/*Load the list of users from the accounts.json file*/
 func loadUserList() {
 	bytevalue, err := os.ReadFile("db/accounts.json")
 	if err != nil {
@@ -35,15 +38,20 @@ func loadUserList() {
 	json.Unmarshal(bytevalue, &User_list)
 }
 
+/*
+Check if the username exists in the userlist, if yes it returns true and
+the index of that user in the list
+*/
 func UserExists(userlist []User, username string) (bool, int) {
 	for index, elements := range userlist {
 		if elements.Name == username {
 			return true, index
 		}
 	}
-	return false, 0
+	return false, -1
 }
 
+/*Save the Current_User points into his corresponding user data in the User_list*/
 func savePoints() {
 	User_list.List[IndexUserList].Points = Current_User.Points
 }
