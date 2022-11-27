56dffcb94fb2fa17bec76c6012fa921d6be4415b
TomJegou <tomjegou10@gmail.com>
2022-11-27 16:45:50 +0100

    force the user tu be logged to navigate into the app

--- src/handler.go
+++ src/handler.go
@@ -37,46 +37,54 @@ var UsedLettersChan = make(chan []string, 1)
 
 /*This HandlerFunc is used to establish a connection and communicate with the hangman goroutine*/
 func hangHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("static/html/hangmanweb.html")
-	if r.Method == "GET" {
-		if runningHangmanCount == 0 {
-			go src.Hangman(InputChan, ResponseChan, LevelChan, AttemptChan, WordChan, QuitChan, UsedLettersChan)
-			runningHangmanCount = 1
+	if Logged {
+		t, _ := template.ParseFiles("static/html/hangmanweb.html")
+		if r.Method == "GET" {
+			if runningHangmanCount == 0 {
+				go src.Hangman(InputChan, ResponseChan, LevelChan, AttemptChan, WordChan, QuitChan, UsedLettersChan)
+				runningHangmanCount = 1
+			}
+			Data.Level = r.FormValue("lvl")
+			LevelChan <- Data.Level
+			Data.WordToDisplay = <-ResponseChan
+			InputChan <- "b0c9713aa009f4fcf39920d0d7eda80714b0c44ff2f98205278be112c755ca45e5386cbe7a9fca360ad22f06e45f80a8b8f23838725d15f889e202f5cea26359"
+			Data.UsedLetters = <-UsedLettersChan
+			InputChan <- "b0c9713aa009f4fcf39920d0d7eda80714b0c44ff2f98205278be112c755ca45e5386cbe7a9fca360ad22f06e45f80a8b8f23838725d15f889e202f5cea26359"
+			Data.Attempt = <-AttemptChan
+			t.Execute(w, Data)
+		} else if r.Method == "POST" {
+			InputChan <- r.FormValue("input")
+			Data.UsedLetters = <-UsedLettersChan
+			Data.WordToDisplay = <-ResponseChan
+			Data.Attempt = <-AttemptChan
+			if Data.WordToDisplay == "50536101b1c465eafbecc8fca26eeb18a2ac8a2f83570bade315c5a112363cdfd820acad2ab234f91d43f0db8fed0cec400a1109ad8f99c21b5b74f59e8bb00d" {
+				fmt.Println("Win")
+				http.Redirect(w, r, "/win", http.StatusFound)
+			} else if Data.WordToDisplay == "889ce65f137b3b9aa1005f417d7972c948b8bb6360cbdd4118cb05a29d37905744fc0dbc3d17c1de02689d837bfea5bb8114a994f9c1a53dddb993139ab2974c" {
+				fmt.Println("Lose")
+				Data.Word = <-WordChan
+				http.Redirect(w, r, "/lose", http.StatusFound)
+			}
+			t.Execute(w, Data)
 		}
-		Data.Level = r.FormValue("lvl")
-		LevelChan <- Data.Level
-		Data.WordToDisplay = <-ResponseChan
-		InputChan <- "b0c9713aa009f4fcf39920d0d7eda80714b0c44ff2f98205278be112c755ca45e5386cbe7a9fca360ad22f06e45f80a8b8f23838725d15f889e202f5cea26359"
-		Data.UsedLetters = <-UsedLettersChan
-		InputChan <- "b0c9713aa009f4fcf39920d0d7eda80714b0c44ff2f98205278be112c755ca45e5386cbe7a9fca360ad22f06e45f80a8b8f23838725d15f889e202f5cea26359"
-		Data.Attempt = <-AttemptChan
-		t.Execute(w, Data)
-	} else if r.Method == "POST" {
-		InputChan <- r.FormValue("input")
-		Data.UsedLetters = <-UsedLettersChan
-		Data.WordToDisplay = <-ResponseChan
-		Data.Attempt = <-AttemptChan
-		if Data.WordToDisplay == "50536101b1c465eafbecc8fca26eeb18a2ac8a2f83570bade315c5a112363cdfd820acad2ab234f91d43f0db8fed0cec400a1109ad8f99c21b5b74f59e8bb00d" {
-			fmt.Println("Win")
-			http.Redirect(w, r, "/win", http.StatusFound)
-		} else if Data.WordToDisplay == "889ce65f137b3b9aa1005f417d7972c948b8bb6360cbdd4118cb05a29d37905744fc0dbc3d17c1de02689d837bfea5bb8114a994f9c1a53dddb993139ab2974c" {
-			fmt.Println("Lose")
-			Data.Word = <-WordChan
-			http.Redirect(w, r, "/lose", http.StatusFound)
-		}
-		t.Execute(w, Data)
+	} else {
+		http.Redirect(w, r, "/login", http.StatusFound)
 	}
 }
 
 /*Handle the selection levels before starting the hangman*/
 func levelHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("static/html/ChoiceLvl.html")
-		t.Execute(w, Data)
-		if runningHangmanCount == 1 {
-			QuitChan <- true
-			runningHangmanCount = 0
+	if Logged {
+		if r.Method == "GET" {
+			t, _ := template.ParseFiles("static/html/ChoiceLvl.html")
+			t.Execute(w, Data)
+			if runningHangmanCount == 1 {
+				QuitChan <- true
+				runningHangmanCount = 0
+			}
 		}
+	} else {
+		http.Redirect(w, r, "/login", http.StatusFound)
 	}
 }
 
